9fd08e5b427072fe94d131b5f2703b64bc75d0bd
Doug Addy <doug.addy@ansys.com>
2023-02-02 13:58:27 +0000

    Remove unnecessary import

--- src/ansys/materials/manager/__init__.py
+++ src/ansys/materials/manager/__init__.py
@@ -1,6 +1,5 @@
 """Provides a helper to manage materials in the Ansys ecosystem."""
 
 from importlib import metadata as metadata
-import sys
 
 __version__ = metadata.version("ansys-materials-manager")
