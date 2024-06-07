475c78fbb58aed954aec0596d28b69ed3af6b3da
Doug Addy <da1910@users.noreply.github.com>
2024-06-07 10:44:47 +0100

    FEAT - 198 - Parse MatML file from a string (#222)
    * Add static methods to read a string and a file, mark the old method as deprecated
    
    * Review comment
    
    - Add properties for deprecated attributes

--- src/ansys/materials/manager/util/matml/matml_parser.py
+++ src/ansys/materials/manager/util/matml/matml_parser.py
@@ -1,7 +1,8 @@
 """Provides the ``matml_parser`` module."""
 from dataclasses import dataclass
 import os
-from typing import Any, Dict, Union
+from typing import Any, Dict, List, Optional, Union
+import warnings
 import xml.etree.ElementTree as ET
 
 _PATH_TYPE = Union[str, os.PathLike]
@@ -44,7 +45,7 @@ class PropertySet:
 
 class MatmlReader:
     """
-    Parse a MATML (engineering data xml) file.
+    Parse a MatML (engineering data xml) file.
 
     Fills a nested dict with all the materials and their properties.
     The key of the first layer are the material names.
@@ -53,24 +54,27 @@ class MatmlReader:
     The data can be accessed via matml_reader.materials
     """
 
-    materials: Dict
-    transfer_ids = Dict
-    matml_file_path: _PATH_TYPE
+    _materials: Dict
+    _transfer_ids = Dict
+    _matml_file_path: _PATH_TYPE
 
     def __init__(self, file_path: _PATH_TYPE):
         """
-        Create a new MATML reader object.
+        Create a new MatML reader object.
 
         Parameters
         ----------
         file_path :
-            Matml (engineering data xml) file path
+            MatML (engineering data xml) file path
         """
-        self.matml_file_path = file_path
+        self._matml_file_path = file_path
+        self._materials = {}
+        self._transfer_ids = {}
         if not os.path.exists(file_path):
             raise RuntimeError(f"Cannot initialize MatmlReader {file_path}. File does not exist!")
 
-    def _convert(self, data: str, target: str) -> Union[str, float]:
+    @staticmethod
+    def _convert(data: str, target: str) -> Union[str, float, List[float]]:
         # convert a string into a certain format (e.g. float)
 
         if target == "string":
@@ -88,7 +92,8 @@ class MatmlReader:
         else:
             return float(data)
 
-    def _read_metadata(self, metadata_node: Any) -> Dict:
+    @staticmethod
+    def _read_metadata(metadata_node: Any) -> Dict:
         # Read the metadata
 
         data = {}
@@ -113,21 +118,23 @@ class MatmlReader:
 
         return data
 
-    def _read_qualifiers(self, property_node: Any) -> Dict:
+    @staticmethod
+    def _read_qualifiers(property_node: Any) -> Dict:
         # returns the qualifiers such as behavior, interpolation options etc.
         qualifiers = {}
         for item in property_node.findall("Qualifier"):
             qualifiers[item.attrib["name"]] = item.text
         return qualifiers
 
-    def _read_property_sets_and_parameters(self, bulkdata: Any, metadata_dict: Dict) -> Dict:
+    @staticmethod
+    def _read_property_sets_and_parameters(bulkdata: Any, metadata_dict: Dict) -> Dict:
         prop_dict = {}
 
         # iterate over the property sets
         for prop_data in bulkdata.findall("PropertyData"):
             property_key = prop_data.attrib["property"]
             property_name = metadata_dict[property_key]["Name"]
-            prop_set_qualifiers = self._read_qualifiers(prop_data)
+            prop_set_qualifiers = MatmlReader._read_qualifiers(prop_data)
 
             parameters = {}
 
@@ -136,8 +143,8 @@ class MatmlReader:
                 parameter_key = parameter.attrib["parameter"]
                 parameter_name = metadata_dict[parameter_key]["Name"]
                 parameter_format = parameter.attrib["format"]
-                param_qualifiers = self._read_qualifiers(parameter)
-                data = self._convert(parameter.find("Data").text, parameter_format)
+                param_qualifiers = MatmlReader._read_qualifiers(parameter)
+                data = MatmlReader._convert(parameter.find("Data").text, parameter_format)
 
                 parameters[parameter_name] = Parameter(
                     name=parameter_name, data=data, qualifiers=param_qualifiers
@@ -149,21 +156,20 @@ class MatmlReader:
 
         return prop_dict
 
-    def _read_materials(self, matml_doc_node: Any, metadata_dict: Dict) -> Dict:
-        # Import the material properties and parameters
-
+    @staticmethod
+    def _read_materials(matml_doc_node: Any, metadata_dict: Dict) -> Dict:
         materials = {}
         for material in matml_doc_node.findall("Material"):
             bulkdata = material.find("BulkDetails")
             name = bulkdata.find("Name").text
-            data = self._read_property_sets_and_parameters(bulkdata, metadata_dict)
+            data = MatmlReader._read_property_sets_and_parameters(bulkdata, metadata_dict)
             materials[name] = data
 
         return materials
 
-    def _read_transfer_ids(self, root: ET.Element) -> int:
-        # reads the material transfer IDs
-        self.transfer_ids = {}
+    @staticmethod
+    def _read_transfer_ids(root: ET.Element, materials: Dict[str, Dict]) -> Dict[str, str]:
+        transfer_ids = {}
         wb_transfer_element = root.find(WBTRANSFER_KEY)
         if wb_transfer_element:
             materials_element = wb_transfer_element.find(MATERIALS_ELEMENT_KEY)
@@ -171,22 +177,48 @@ class MatmlReader:
                 mat_name = mat.find("Name").text
                 transfer_id_element = mat.find(MAT_TRANSFER_ID)
 
-                if not mat_name in self.materials.keys():
+                if not mat_name in materials.keys():
                     raise RuntimeError(f"Transfer ID could not be set for material {mat_name}")
 
-                self.transfer_ids[mat_name] = transfer_id_element.text
+                transfer_ids[mat_name] = transfer_id_element.text
 
-        return len(self.transfer_ids)
+        return transfer_ids
 
-    def parse_matml(self) -> bool:
-        """Read MATML (engineering data XML) file.
+    @staticmethod
+    def parse_from_file(file_path: _PATH_TYPE) -> Dict[str, Union[str, Dict]]:
+        """Read MatML (engineering data XML) data from a file.
 
-        Output can be consumed via matml_reader.materials or
-        matml_reader.get_material(name).
+        Returns the material information and the workbench transfer identities (if present).
 
-        Returns the number of imported materials.
+        Parameters
+        ----------
+        file_path: Union[str, Path]
+            Path to MatML file on disk
+
+        Returns
+        -------
+        Dict[str, Union[str, Dict]]
         """
-        tree = ET.parse(self.matml_file_path)
+        with open(file_path, "r", encoding="utf8") as fp:
+            file_content = fp.read()
+        return MatmlReader.parse_text(file_content)
+
+    @staticmethod
+    def parse_text(matml_content: str) -> Dict[str, Union[str, Dict]]:
+        """Read MatML (engineering data XML) data from a string.
+
+        Returns the material information and the workbench transfer identities (if present).
+
+        Parameters
+        ----------
+        matml_content: str
+            MatML content in text form
+
+        Returns
+        -------
+        Dict[str, Union[str, Dict]]
+        """
+        tree = ET.ElementTree(ET.fromstring(matml_content))
         root = tree.getroot()
         materials_node = root.find(MATERIALS_ELEMENT_KEY)
         if not materials_node:
@@ -203,19 +235,121 @@ class MatmlReader:
             raise RuntimeError(
                 "Metadata node not found. Please check if this is a valid MATML file."
             )
-        metadata_dict = self._read_metadata(metadata_node)
+        metadata_dict = MatmlReader._read_metadata(metadata_node)
 
-        self.materials = self._read_materials(matml_doc_node, metadata_dict)
+        materials = MatmlReader._read_materials(matml_doc_node, metadata_dict)
+        transfer_ids = MatmlReader._read_transfer_ids(root, materials)
+        return {
+            material_id: {
+                "material": materials[material_id],
+                "transfer_id": transfer_ids[material_id],
+            }
+            for material_id in materials
+        }
 
-        self._read_transfer_ids(root)
-        return len(self.materials)
+    def parse_matml(self) -> int:
+        """Read MATML (engineering data XML) file.
+
+        Output can be consumed via matml_reader.materials or
+        matml_reader.get_material(name).
+
+        Returns
+        -------
+        int: Number of imported materials.
+
+        .. deprecated:: 0.2.3
+          `parse_matml` will be removed in version 0.3.0, it is replaced by
+          `parse_from_file` and `parse_from_text`.
+        """
+        warnings.warn(
+            "parse_matml will be removed in version 0.3.0, use the static methods "
+            "`parse_from_file` and `parse_from_text` instead",
+            DeprecationWarning,
+        )
+
+        parsed_data = MatmlReader.parse_from_file(self._matml_file_path)
+        self._materials = {k: v["material"] for k, v in parsed_data.items()}
+        self._transfer_ids = {k: v["transfer_id"] for k, v in parsed_data.items()}
+        return len(self._materials)
+
+    @property
+    def materials(self) -> Optional[Dict]:
+        """Return the parsed material data from the MatML file.
+
+        Property will be None unless the parser has successfully parsed a MatML file.
+
+        Returns
+        -------
+        Dict: Material data from the MatML file
+
+        .. deprecated:: 0.2.3
+           `materials` will be removed in version 0.3.0, instead use the static methods
+           `parse_from_file` and `parse_from_text` to parse the MatML file and
+           obtain the parsed material dictionary.
+        """
+        warnings.warn(
+            "materials will be removed in version 0.3.0, instead use the static methods "
+            "`parse_from_file` and `parse_from_text` to parse the MatML file.",
+            DeprecationWarning,
+        )
+        return self._materials
+
+    @property
+    def transfer_ids(self) -> Optional[Dict[str, str]]:
+        """Return the parsed Workbench Transfer IDs from the MatML file.
+
+        Property will be None unless the parser has successfully parsed a MatML file.
+
+        Returns
+        -------
+        Dict: Workbench transfer IDs from the MatML file
+
+        .. deprecated:: 0.2.3
+           `transfer_ids` will be removed in version 0.3.0, instead use the static methods
+           `parse_from_file` and `parse_from_text` to parse the MatML file and
+           obtain the Workbench transfer IDs.
+        """
+        warnings.warn(
+            "transfer_ids will be removed in version 0.3.0, instead use the static methods "
+            "`parse_from_file` and `parse_from_text` to parse the MatML file.",
+            DeprecationWarning,
+        )
+        return self._transfer_ids
+
+    @property
+    def matml_file_path(self) -> str:
+        """Return the path to the target MatML file.
+
+        .. deprecated:: 0.2.3
+           `matml_file_path` will be removed in version 0.3.0, instead use the static methods
+           `parse_from_file` and `parse_from_text` to parse the MatML file and
+           obtain the parsed material dictionary and the Workbench transfer IDs.
+        """
+        warnings.warn(
+            "matml_file_path will be removed in version 0.3.0, instead use the static methods "
+            "`parse_from_file` and `parse_from_text` to parse the MatML file.",
+            DeprecationWarning,
+        )
+        return self._matml_file_path
 
     def get_material(self, name: str) -> Dict:
-        """Return a certain material."""
-        if not name in self.materials.keys():
-            available_keys = ", ".join(self.materials.keys())
+        """Return a certain material.
+
+        .. deprecated:: 0.2.3
+          `get_material` will be removed in version 0.3.0, instead use the static methods
+          `parse_from_file` and `parse_from_text` to parse the MatML file and
+          obtain the parsed material dictionary and the Workbench transfer ID.
+        """
+        warnings.warn(
+            "get_material will be removed in version 0.3.0, instead use the static methods "
+            "`parse_from_file` and `parse_from_text` to parse the MatML file.",
+            DeprecationWarning,
+        )
+
+        if name not in self._materials.keys():
+            available_keys = ", ".join(self._materials.keys())
             raise RuntimeError(
                 f"Material {name} does not exist. Available materials are {available_keys}"
             )
 
-        return self.materials[name]
+        return self._materials[name]
